10f7865514648931e7effce7be5c500fad26c84d
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-06 17:15:53 +0200

    adding cards and styles

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,13 +1,51 @@
 <template>
-  <div class="card">Card Here</div>
+    <div class="card">
+    <img :src="details.poster" :alt="details.title">
+    <h2>{{details.title}}</h2>
+    <h3>{{details.author}}</h3>  
+    <h3>{{details.year}}</h3>
+    <h4>{{details.genre}}</h4> 
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Cards',
+    props: ['details'],
 }
 </script>
 
 <style scoped lang="scss">
+
+.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 300px;
+    flex-basis: calc(100% / 8);
+    background: #2e3a46;
+    margin: 0 1rem;
+    padding: 1rem;
+}
+
+.card img {
+    max-width: 150px;
+}
+
+h2 {
+    font-size: 18px;
+    margin: 12px 0;
+}
+
+h3 {
+    font-size: 16px;
+    margin-bottom: 10px;
+}
+
+h4 {
+    font-size: 14px;
+}
 
 </style>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,25 +1,66 @@
 <template>
   <main class="content">
-      <Cards />
+    <div v-for="(card, index) in musicCards" :key="index" class="cardList">
+      <Cards :details="card" />
+    </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
 import Cards from '@/components/Cards.vue';
 
 export default {
     name: 'Content',
     components: {
         Cards,
-    }
+    },
+    data () {
+        return {
+            apiURL: 'https://flynn.boolean.careers/exercises/api/array/music',
+            musicCards: [],
+            loading: true,
+        }
+    },
+    created() {
+        this.getMusicCard();
+    },
+    methods: {
+        getMusicCard() {
+            /**
+             * API CALL
+             */
+            axios.get(this.apiURL)
+                // SUCCESS
+                .then(res => {
+                    this.musicCards = res.data.response;
+                    console.log(this.musicCards);
+                    this.loading = false;
+                })
+                // FAIL
+                .catch( err => {
+                    console.log('Error', err);
+                    alert('Error',err);
+                });
+        },
+    },
 }
 </script>
 
 <style scoped lang="scss">
 
 .content {
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 100px;
     background:  #1d2d3c;
     height: calc(100vh - 64px);
+}
+
+.cardList:nth-child(9),
+.cardList:nth-child(10) {
+    position: relative;
+    left: 36.6%;
 }
 
 </style>
